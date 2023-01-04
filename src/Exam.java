9c3be62c91fae15e1de9581fe1aef077604e8a39
MajidTal <MajidAlhadidi@outlook.com>
2023-01-04 10:34:23 +0400

    searching in pdf

--- src/Exam.java
+++ src/Exam.java
@@ -6,74 +6,91 @@ import java.util.Scanner;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.PdfWriter;
- 
+import com.itextpdf.text.pdf.parser.PdfTextExtractor;
+
 public class Exam {
 
-	public static void main(String[] args) throws IOException{
-		Scanner a= new Scanner(System.in);
-		String word;
+	public static void main(String[] args) throws IOException {
+		Scanner a = new Scanner(System.in);
 		try {
-			for(int i=0; i<= 20; i++) {
-		//Create Document instance.
-		Document document = new Document();
-	 
-		//Create OutputStream instance.
-		FileOutputStream outputStream = new FileOutputStream(new File("test1" + i + ".pdf"));
-	 
-		//Create PDFWriter instance.
-	        PdfWriter.getInstance(document, outputStream);
-	 
-	        //Open the document.
-	        document.open();
-	 
-	        //Add content to the document.
-	        document.add(new Paragraph(" Welcome,This is Majid Page "));
-	     
-	        //Close document and outputStream.
-	        document.close();
+			for (int i = 0; i <= 100; i++) {
+				// Create Document instance.
+				Document document = new Document();
+
+				// Create OutputStream instance.
+				FileOutputStream outputStream = new FileOutputStream(new File("test" + i + ".pdf"));
+
+				// Create PDFWriter instance.
+				PdfWriter.getInstance(document, outputStream);
+
+				// Open the document.
+				document.open();
+
+				// Add content to the document.
+				if (i <= 20) {
+					document.add(new Paragraph(" Welcome,This is Majid Page "));
+
+				}
+				else if (i <=30)
+				{
+					document.add(new Paragraph(" Hi,This is my second Page   "));
+					
+				} 
+				else if (i <= 50) 
+				{
+					document.add(new Paragraph(" Helow,This is the third Page "));
+				} 
+				else
+
+					document.add(new Paragraph(" Hi there,This is the forth Page "));
+
+				document.close();
+				outputStream.close();
+
 			}
-	 
-	        System.out.println("Pdf created successfully.");
-	    } catch (Exception e) {
-		e.printStackTrace();
-	    }
+
+			System.out.println("Pdf created successfully.");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
-		  String q = a.next();
-		  System.out.println("Ahmed");
-		  String qq = a.next();
-		  System.out.println("mohammed");
-		  String qqq = a.next();
-		  System.out.println("Ali");
-		  String w = a.next();
-		  System.out.println("azzoz");
-		  String ww = a.next();
-		  System.out.println("adnan");
-		  String www = a.next();
-		  System.out.println("Majid");
-	
-	      File directoryPath=new File("C:\\Users\\Lenovo\\eclipse-workspace\\FristExam");
-	      File filesList[] =directoryPath.listFiles();
-		 System.out.println("Enter the word to be search in pdf file");
-	     	word = a.next();
-	     	for(File file : filesList)
-	     	{{
-	     		System.out.println("File name: " + file.getName());
-	     		try {
-	     			pdfReader pdfReader = new pdfReader(file.getPath());
-	     		
-	
-	     		}
-	     		}
-	     	}
-	     	
-	     	
-	     	
-	     	
-	     	
-	     	
-	     	
-	     	
-	     	
-	     	
-	     	
+		String path = "C:\\Users\\Lenovo\\eclipse-workspace\\FristExam";
+			System.out.println("search the word");
+			String searchWord =a.next();
+			File pt1 = new File(path);
+			File[] pdfFiles = pt1.listFiles((d,name) -> name.endsWith(".pdf"));
+			
+			for(File pdfFile : pdfFiles)
+			try {
+				PdfReader reader = new PdfReader (pdfFile.getAbsolutePath());
+				int numPages = reader.getNumberOfPages();
+			boolean found = false;
+			for(int i =1; i<=numPages; i++ )
+			{
+				String pageText = PdfTextExtractor.getTextFromPage(reader, i);
+				if(pageText.toLowerCase().contains(searchWord.toLowerCase())) {
+					found = true;
+					break;
+				}
+				}
+			
+			    if(found)
+			    {
+			    	System.out.println(" Found " + searchWord + " in " + pdfFile.getName());
+			    	}
+			    }
+				catch (Exception e)
+			{
+					e.printStackTrace();
+			}
+		}
+}
+		
+		
+		
+
+
+
+
